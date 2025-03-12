715425770e7736c9c0ffdc173f00969532c62e1a
datprs <datprs@gmail.com>
2025-03-12 16:39:05 +0700

    Fordward Discord

--- test.js
+++ test.js
@@ -3,21 +3,45 @@ require('dotenv').config();
 const { Api, TelegramClient } = require('telegram');
 const { StringSession } = require('telegram/sessions');
 const input = require('input'); // npm i input
+const { NewMessage } = require('telegram/events');
+const { Client, GatewayIntentBits } = require('discord.js');
 const apiId = process.env.API_ID;
 const apiHash = process.env.API_HASH;
+const discordToken = process.env.DISCORD_TOKEN;
+
+const client = new TelegramClient(
+  new StringSession(
+    '1BQANOTEuMTA4LjU2LjE5MwG7tlgXlwev4JcJ5mDFjjitXZDK5xWNDZf7xjFUfzkRoIuRpLOOfETwRunQVh3X77Ej03v7fxU7Hm1abniZ5JDmGgRC6dmU14iaKWuv2LRohnGXcbSHDIX5v1u1L4NIidqv5ku/gvYv57nkqHpXFHjTSIVf7YOu2L0r9IAAaoeey6Hje7js5cRIkeH3yosnklZOO9O+WKzGWZtKhAJyuaTnVnXl1eekFa/Zv0LDn/x4U8M0urm1F75EINuppIGFQmI824Ajf25a3v/5sveUT7VDVrxR25C64nuOONRT2uzMl/CiQzp6om+/5Psm3S7B1jcDglzke1GzpxCyDnHMmEJVzw=='
+  ),
+  parseInt(apiId),
+  apiHash
+);
+
+const discord = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
+
+let discordChannel;
+
+discord.on('ready', async () => {
+  console.log(`> Bot is on ready`);
+  const channel = await discord.channels.fetch('1349310183502905428');
+  discordChannel = channel;
+});
+
+discord.login(discordToken);
 
 (async () => {
-  const client = new TelegramClient(
-    new StringSession(
-      '1BQANOTEuMTA4LjU2LjE5MwG7Z2GdLkGx7bzH6XdUJJdl2F3IjXtZDJODNMt1UG8QVR4Hl8BrZX5jwlmElMYOjxuIBGHkTV7JAzhyLhIFvoybAVL7PDYPPzkPfaFrJC2herpaxcgVAS4IJ9VAndQnRCcMPFIgcsLndT/1+2khWPqn8coGI738x+42y20bCxy2sM56oB5G5I24ehoZUbUq3tqtFLpv9I08ZvurW4tNjJeYZd+99xM2p/nbCFAHVhm+m2wkZ06Ob3wKZdkP+nvElrMhaotrny06cDnHA6UpdASf7/of7UMOJp5qXOgKqRA+5SFPI0hvfYIHkHZiNpwjZuVoZAkyAMsDp6/FE4xA4b70vw=='
-    ),
-    parseInt(apiId),
-    apiHash
-  );
-
   await client.connect();
-
-  let dialogs = await client.getDialogs();
-
-  console.log(dialogs);
+  client.addEventHandler(
+    eventPrint,
+    new NewMessage({
+      chats: ['-1002493740338'],
+    })
+  );
 })();
+
+async function eventPrint(event) {
+  const message = event.message;
+  discordChannel.send(message.text);
+}
